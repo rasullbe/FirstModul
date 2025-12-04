51a270777dba223254c83df3545a853007438634
rasullbe <rasulbek112208@gmail.com>
2025-12-04 13:40:12 +0500

    2darsHomework

--- Dars2/2dars/2dars/Program.cs
+++ Dars2/2dars/2dars/Program.cs
@@ -4,7 +4,117 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //FirstFile
+            //1
+            //int intValue = 34;
+            //int FirstRoom = intValue % 10;
+            //Console.WriteLine(FirstRoom);
+
+            //2
+            //int intValue = 123;
+            //int SecondRoom = intValue % 100;
+            //int mainSecondRoom = SecondRoom / 10;
+            //Console.WriteLine(mainSecondRoom);
+
+            //3
+            //int intValue = 123;
+            //int ThirdRoom = intValue / 100;
+            //Console.WriteLine(ThirdRoom);
+
+            //4
+            //int intValue = 12345;
+            //int BeshRoom = intValue % 10000;
+            //int TheAnswer = BeshRoom / 1000;
+            //Console.WriteLine(TheAnswer);
+
+            //5
+            //int intValue = 1234;
+            //int FourthRoom = intValue % 1000;
+            //int theAnswer = FourthRoom / 100;
+            //Console.WriteLine(theAnswer);
+
+            //6
+            //int intValue = 127;
+            //int FirstRoom = intValue / 100;
+            //int fakeSecondRoom = intValue % 100;
+            //int secondRoom = fakeSecondRoom / 10;
+            //int thirdRoom = fakeSecondRoom % 10;
+            //Console.WriteLine(FirstRoom + secondRoom + thirdRoom);
+
+            //7
+            //int intValue = 127;
+            //int FirstRoom = intValue / 100;
+            //int fakeSecondRoom = intValue % 100;
+            //int secondRoom = fakeSecondRoom / 10;
+            //int thirdRoom = fakeSecondRoom % 10;
+            //Console.WriteLine(FirstRoom * secondRoom * thirdRoom);
+
+            //8
+            //int intValue = 53;
+            //int theFirstRoom = intValue / 10;
+            //int theSecondRoom = intValue % 10;
+            //Console.WriteLine(theFirstRoom * theSecondRoom);
+
+            //9
+            //int intValue = 12345;
+            //int type da 1 ta 5 xonali son berilgan.Shu sonni raqamlar yig'indisini aniqlab ekranga chiqaring.
+
+            //10
+            //???
+
+            //SecondFile
+            //1
+            //int intValue = 2;
+            //int stepenChisla = intValue * intValue;
+            //int stepenChislaFour = stepenChisla * stepenChisla;
+            //Console.WriteLine(stepenChislaFour);
+
+            //2
+            //int intValue = 2;
+            //int wayToSixthStepen = intValue * intValue;
+            //int secondWayToSixthStepen = wayToSixthStepen * intValue;
+            //int SixthStepen = secondWayToSixthStepen * secondWayToSixthStepen;
+            //Console.WriteLine(SixthStepen);
+
+            //3
+            //int intValue = 2;
+            //int wayToEight = intValue * intValue;
+            //int secondWayToEight = wayToEight * wayToEight;
+            //int realStepenOfEight = secondWayToEight * secondWayToEight;
+            //Console.WriteLine(realStepenOfEight);
+
+            //4
+            //int days = 5;
+            //int hours = 24;
+            //int daysToHours = days * 24;
+            //Console.WriteLine(daysToHours + hours);
+
+            //5
+            //int weeks = 5;
+            //int hours = 24;
+            //int weeksToHours = weeks * 168;
+            //Console.WriteLine(weeksToHours + hours);
+
+            //6
+            //int minutes = 5;
+            //int hours = 10;
+            //int minutesToSeconds = minutes * 60;
+            //int hoursToSeconds = hours * 360;
+            //int OverallSeconds = minutesToSeconds + hoursToSeconds;
+            //Console.WriteLine(OverallSeconds);
+
+            //7
+            //int FileXajm = 5000;
+            //double Kilobite = FileXajm / 1024.0;
+            //Console.WriteLine(Kilobite);
+
+            //8
+            //double speedKB = 750; 
+            //double fileGB = 1.8;
+            //double fileKB = fileGB * 1024 * 1024; 
+            //double seconds = fileKB / speedKB;
+
+            //Console.WriteLine(seconds);
         }
     }
 }
