9fcd6cda415fc96743a7cf0e8805aea113472d62
rasullbe <rasulbek112208@gmail.com>
2025-12-05 14:52:25 +0500

    thirdLesson

---
+++ Dars3/Dars3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dars3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Avalue = 17;
+            var Bvalue = 23;
+
+            for (int i = 1;  i <= Avalue; i++)
+            {
+
+            }
+        }
+    }
+}
