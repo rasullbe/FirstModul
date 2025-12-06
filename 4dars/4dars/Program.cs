902d6d3b9e0d208563831ece9f87fb5bacaf724f
rasullbe <rasulbek112208@gmail.com>
2025-12-06 20:34:18 +0500

    4lesson

---
+++ 4dars/4dars/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _4dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string RandomText = "I love niggers";
+            var TheAnswer = RandomText.Substring(11);
+            Console.WriteLine(TheAnswer);
+
+            //2
+            string RandomText2 = "RasulbekMatrasulov";
+            var TheAnswer2 = RandomText2.Substring(2, 14);
+            Console.Write(TheAnswer2);
+        }
+    }
+}
