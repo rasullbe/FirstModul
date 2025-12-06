9c7e02bbfc31aa020e264f836c22d0e2dab4d597
rasullbe <rasulbek112208@gmail.com>
2025-12-06 12:50:31 +0500

    homework

--- Dars3/Dars3/Program.cs
+++ Dars3/Dars3/Program.cs
@@ -4,13 +4,158 @@
     {
         static void Main(string[] args)
         {
-            var Avalue = 17;
-            var Bvalue = 23;
+            //FirstFile
 
-            for (int i = 1;  i <= Avalue; i++)
+            //1
+            //for (int i = 1; i <=20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //2
+            //for (int i = 25; i <= 100; i++)
+            //{
+            //    Console.Write(i);
+            //}
+
+            //3
+            //for (int i = 12; i <= 200; i++)
+            //{
+            //    Console.Write(i);
+            //}
+
+            //4
+            //for (int i = 15; i >= 2; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //5
+            //for (int i = 120; i >= 23; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //6
+            //for (int i = 10; i <= 50; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //7
+            //for (int i = 120; i <= 250; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //8
+            //for (int i = 100; i <= 50; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //9
+            //for (int i = 80; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //10
+            //int aValue = 10; 
+            //int bValue = 50; 
+
+            //for (int i = aValue; i <= bValue; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //11
+            //int a = 1;   
+            //int b = 200; 
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 4 == 0 && i % 7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //12
+            //int a = 10;  
+            //int b = 100; 
+
+            //int count = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 8 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"{count}");
+
+
+            //13
+            //int a = 10;  
+            //int b = 100; 
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if (i % 9 == 0 || i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //14
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0) 
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+            //Console.WriteLine($"{sum}");
+
+
+            //15
+            int intValue = 911;
+            int r1 = intValue % 10;
+            int r2 = intValue / 100;
+            int r3 = intValue % 100 / 10;
+
+            int summOfNumbers = r1 + r2 + r3;
+            if (summOfNumbers > 20)
             {
-
+                Console.Write(summOfNumbers);
+            } else
+            {
+                Console.Write("20 dan kam");
             }
+
+            //16
+            
+
+            
         }
     }
 }
