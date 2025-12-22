6d000a57594a7b020c9fd55a2ff62fa82c122820
rasullbe <rasulbek112208@gmail.com>
2025-12-22 23:04:11 +0500

    homework

---
+++ 11dars/11dars/Program.cs
@@ -0,0 +1,30 @@
+﻿using Dars11;
+
+namespace _11dars;
+
+internal class Program
+{
+    static List<Employee> employees = new List<Employee>();
+    static void Main(string[] args)
+    {
+
+    }
+
+    static void AddEmployee(Employee emp)
+    {
+        employees.Add(emp);
+    }
+
+    static void RemoveEmployee(Guid GuidId)
+    {
+        foreach (var employer in employees)
+        {
+            if (employer.GuidId == GuidId)
+            {
+
+            }
+        }
+    }
+
+    
+}
---
+++ 11dars/11dars/School.cs
@@ -0,0 +1,10 @@
+﻿namespace _11dars;
+
+internal class School
+{
+    public string SchoolName {  get; set; }
+    public int SchoolId { get; set; }
+    public string SchoolLocation { get; set; }
+    public int SchoolCapacity { get; set; }
+    public int SchoolCurrentCount { get; set; }
+}
