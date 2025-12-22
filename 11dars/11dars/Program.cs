64374a8443e1c72a0cbbdc5396805e9399d0f3bf
rasullbe <rasulbek112208@gmail.com>
2025-12-22 23:32:25 +0500

    homework

--- 11dars/11dars/Program.cs
+++ 11dars/11dars/Program.cs
@@ -7,6 +7,44 @@ internal class Program
     static List<Employee> employees = new List<Employee>();
     static void Main(string[] args)
     {
+        Employee employee1 = new Employee
+        {
+            GuidId = Guid.NewGuid(),
+            FirstName = "John",
+            LastName = "Doe",
+            Position = "Developer",
+            Salary = 60000,
+            Age = 30
+        };
+
+        Employee employee2 = new Employee
+        {
+            GuidId = Guid.NewGuid(),
+            FirstName = "Jane",
+            LastName = "Smith",
+            Position = "Designer",
+            Salary = 55000,
+            Age = 28
+        };
+
+        AddEmployee(employee1);
+        AddEmployee(employee2);
+
+        Console.WriteLine("All Employees:");
+        GetAllEmployees();
+
+        var newEmployee = new Employee
+        {
+            GuidId = employee1.GuidId,
+            FirstName = "John",
+            LastName = "Doe",
+            Position = "Senior Developer",
+            Salary = 80000,
+            Age = 31
+        };
+
+        UpdateEmployee(employee1.GuidId, newEmployee);
+        RemoveEmployee(employee2.GuidId);
 
     }
 
@@ -21,10 +59,32 @@ internal class Program
         {
             if (employer.GuidId == GuidId)
             {
-
+                employees.Remove(employer);
+                break;
             }
         }
     }
 
-    
+    static void UpdateEmployee(Guid GuidId, Employee newEmp)
+    {
+        for (int i = 0; i < employees.Count; i++)
+        {
+            if (employees[i].GuidId == GuidId)
+            {
+                employees[i] = newEmp;
+                break;
+            }
+        }
+    }
+
+    static void GetAllEmployees()
+    {
+        foreach (var employer in employees)
+        {
+            Console.WriteLine(employer);
+        }
+    }
 }
+
+
+
